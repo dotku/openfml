b1be1695fd510a31061fc1352674ee144ab1cc26
Jay Lin <weijingjaylin@gmail.com>
2016-05-05 18:40:02 -0700

    reconstruction

---
+++ lab/tpqmg/app/Runtime/Cache/Home/2bc71d220ff77e6b12bf354b9e8a67ea.php
@@ -0,0 +1,17 @@
+<?php if (!defined('THINK_PATH')) exit();?><html>
+    <head>
+        <meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>饭米粒 - ThinkPHP + 腾讯云 万象优图案例</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="<?php echo /lab/tencent/tpqmg;?>/bower_components/bootstrap/dist/css/bootstrap.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="<?php echo /lab/tencent/tpqmg;?>/bower_components/jquery/dist/js/jquery.min.js"></script>
+        <script src="<?php echo /lab/tencent/tpqmg;?>/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    </head>
+	<body>
+		
+	</body>
+</html>
