6c1d8bbce88d38e7dd1e31eda447c99eb74272e1
Jay Lin <weijingjaylin@gmail.com>
2016-05-11 01:15:26 -0700

    update common/function

--- html/app/Common/Common/function.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-function fnmili_json_decode($requestURL, $data) {
-    if ($data) {
-        $requestURL = $requestURL . '?' . http_build_query($data);
-    }
-    return json_decode(file_get_contents($requestURL), true);
-}
