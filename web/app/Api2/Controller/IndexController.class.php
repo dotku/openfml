f37a3b6e4ceac58a972fbd01a85c1e17de1f0243
Jay Lin <weijingjaylin@gmail.com>
2016-05-18 02:57:54 -0700

    update home

--- web/app/Api2/Controller/IndexController.class.php
+++ web/app/Api2/Controller/IndexController.class.php
@@ -3,7 +3,7 @@ namespace Api2\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function _initialize(){
-
+        $this->output = array();
     }
     public function index(){
         $white_list = array(
@@ -18,38 +18,52 @@ class IndexController extends Controller {
                 case 'user':
                     $this->_getUser();
                     break;
-                default:
-                    $model = D($_GET['table']);
-                    //var_dump($_GET['id']);
-                    if ($_GET['id']) {
-                        $map['id'] = intval($_GET['id']);
-                        $info = $model->where($map)->find();
-                        if ($info) {
-                            $output['data'] = $info;
-                            $output['status_code'] = 1;
-                        } else {
-                            $output['data'] = 'Either null or empty.';
-                            $output['status_code'] = -1;
-                        }
+                case 'goods_cate':
+                    if (!$_GET['has_goods']) {
+                        $this->_default();
                     } else {
-                        $list = $model->select();
-                        if (is_array($list) && !empty($list)) {
-                            $output['data'] = $list;
-                            $output['status_code'] = 1;
-                        } else {
-                            $output['data'] = 'Either null or empty.';
-                            $output['status_code'] = -1;
-                        }
+                        $this->_goods_cate_hasGoods();
                     }
-                    echo json_encode($output);
+                    break;
+                default:
+                    $this->_default();
             }
+            echo json_encode($this->output);
 
         } else {
-            //var_dump(in_array($_GET['table'], $white_list));
+            // var_dump(in_array($_GET['table'], $white_list));
             echo 'Hello, Welcome to API 2!';
         }
 
     }
+
+    public function _default(){
+        //var_dump($_GET['table']);
+        $model = D($_GET['table']);
+        //var_dump($_GET['id']);
+        if ($_GET['id']) {
+            $map['id'] = intval($_GET['id']);
+            $info = $model->where($map)->find();
+            if ($info) {
+                $this->output['data'] = $info;
+                $this->output['status_code'] = 1;
+            } else {
+                $this->output['data'] = 'Either null or empty.';
+                $this->output['status_code'] = -1;
+            }
+        } else {
+            $list = $model->select();
+            if (is_array($list) && !empty($list)) {
+                $output['data'] = $list;
+                $output['status_code'] = 1;
+            } else {
+                $output['data'] = 'Either null or empty.';
+                $output['status_code'] = -1;
+            }
+        }
+        $this->output = $output;
+    }
+
     public function _getUser(){
         $model = D($_GET['table']);
         $list = $model->order('uid desc')->limit(1000)->select();
@@ -65,6 +79,19 @@ class IndexController extends Controller {
             $output['status_code'] = -1;
         }
         echo json_encode($output);
+    }
+    public function _goods_cate_hasGoods(){
+        $model_goods = D('goods');
+        $model_goods_cate = D('goods_cate');
+        $output_cate_list = array();
+
+        $group_goods = $model_goods->group('cate_id')->select();
+        foreach($group_goods as $key => $val) {
+            $map['id'] = $val['cate_id'];
+            $info_goods_cate = $model_goods_cate->where($map)->find();
+            array_push($output_cate_list, $info_goods_cate);
+        }
+        $this->output = $output_cate_list;
     }
     public function jsonp(){
 
--- web/public/js/Home/Index/angular-index.js
+++ web/public/js/Home/Index/angular-index.js
@@ -8,16 +8,33 @@ var indexApp = angular.module('indexApp', [])
         }
         console.log($scope.brand);
     }, function(rsp){
-        console.log('brandApp request failed');
-        console.log(rsp);
+        // console.log('brandApp request failed');
+        // console.log(rsp);
     });
 
 }]).controller('featuredCtrl',['$scope','$http',function($scope,$http){
     $http.get(__ROOT__ + '/index.php/Api2/goods').success(function(data){
         $scope.goods = data.data;
+        // console.log('featureCtrl goods', $scope.goods);
+    });
+    $http.get(__ROOT__ + '/index.php/Api2/goods_cate?has_goods=1').success(function(data){
+        $scope.goods_cate = data;
+        // console.log('featureCtrl goods_cate', $scope.goods_cate);
     });
 }]).controller('cateCtrl',['$scope','$http',function($scope,$http){
     $http.get(__ROOT__ + '/index.php/Api2/goods_cate').success(function(data){
         $scope.cate = data.data;
     });
-}]);
+}]).filter('filterByCate', function(){
+    return function(goods, gc_id){
+        console.log('filter', gc_id);
+        console.log('filterByCate goods', goods);
+        for (var i = 0; i < goods.length; i++){
+            if (goods[i].cate_id != gc_id){
+                goods.splice(i, 1);
+            }
+        }
+        console.log('filterByCate goods', goods);
+        return goods;
+    }
+});
